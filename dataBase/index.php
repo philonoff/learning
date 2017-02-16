bcb5f81d8c5734cf167a09d71033685fcf62c24e
philonoff <philkrm@gmail.com>
2017-02-16 21:06:03 +0200

    commit

--- dataBase/index.php
+++ dataBase/index.php
@@ -21,9 +21,27 @@ $opt = array(
 
 $pdo = new PDO($dsn, $user, $pass, $opt);
 
-$stmt = $pdo->query("SELECT name FROM region_stage_1");
-while ($row = $stmt->fetch()) {
-    echo $row['name'] . "<br>";
-}
+//для статических запросов query()
+//$stmt = $pdo->query("SELECT name FROM region_stage_1");
+//while ($row = $stmt->fetch()) {
+//    echo $row['name'] . "<br>";
+//}
 
+$levelId = $_POST['level'];
+//$sql = "SELECT name FROM region_stage_1 WHERE school_class = ?";
+//$stmt->execute(array($school_class));
+//$sql = "SELECT name FROM region_stage_1 WHERE school_class = :school_class";
+
+//$stmt = $pdo->prepare("SELECT name,surname,sex,school_class FROM region_stage_1 WHERE level_id = ?");
+//$stmt->execute(array($levelId));
+//foreach ($stmt as $value) {
+//    echo "<pre>";
+//    print_r($value);
+//    echo "</pre>";
+//}
 ?>
+
+<form action="" method="POST">
+    <input type="text" name="level">
+    <input type="submit" name="submit">
+</form>
