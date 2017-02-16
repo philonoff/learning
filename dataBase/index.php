ea99be836648331a34926b357676f867d63c47c2
philonoff <philkrm@gmail.com>
2017-02-16 19:28:49 +0200

    Initial commit

---
+++ dataBase/index.php
@@ -0,0 +1,24 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$host = "localhost";
+$db = "date";
+$charset = "UTF8";
+$user = "root";
+$pass = "";
+
+//источник данных database source name
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+
+//options
+$opt = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+);
+
+$pdo = new PDO($dsn, $user, $pass, $opt);
+
+?>
