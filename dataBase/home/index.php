11c1b621e855a510911719430276245ec17648f2
philonoff <philkrm@gmail.com>
2017-02-18 18:37:41 +0200

    commit

--- dataBase/home/index.php
+++ dataBase/home/index.php
@@ -1,4 +1,24 @@
 <?php
+
+$host = "localhost";
+$db = "date";
+$charset = "UTF8";
+$user = "root";
+$pass = "";
+
+//источник данных database source name
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+//options
+$opt = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+);
+
+$pdo = new PDO($dsn, $user, $pass, $opt);
+
+$stmt = $pdo->query("SELECT id, name, surname FROM region_stage_1");
+
 ?>
 
 <!DOCTYPE html>
@@ -30,14 +50,20 @@
     <div class="row marketing">
         <div class="col-lg-12">
             <table class="table table-hover">
-                <tr>
-                    <td width="85%">-----Новость-----</td>
-                    <td><a class="btn btn-default btn-sm" href="editnews.php?id=1">Редактировать</a></td>
-                </tr>
-                <tr>
-                    <td width="85%">-----Новость-----</td>
-                    <td><a class="btn btn-default btn-sm" href="editnews.php?id=1">Редактировать</a></td>
-                </tr>
+                <?php
+                    $colcount = $stmt->rowCount();
+                    if ($colcount > 0) {
+                        while ($row = $stmt->fetch()) {
+                            $result = "<tr>";
+                            $result .= "<td width='85%'>" . $row['name'] . " " . $row['surname'] . "</td>";
+                            $result .= "<td><a class='btn btn-default btn-sm' href='editnews.php?id={$row['id']}'>Редактировать</a></td>";
+                            $result .= "</tr>";
+                            echo $result;
+                        }
+                    } else {
+                        echo "<h4>Пока нет ни одной новости</h4>";
+                    }
+                ?>
             </table>
         </div>
     </div>
