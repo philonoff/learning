65a14faf595ef6da6ec5c8cc87d86b7dd50c7b08
philonoff <philkrm@gmail.com>
2017-02-18 15:25:09 +0200

    commit

--- dataBase/index.php
+++ dataBase/index.php
@@ -10,7 +10,7 @@ function printr($data) {
 }
 
 $host = "localhost";
-$db = "date";
+$db = "area";
 $charset = "UTF8";
 $user = "root";
 $pass = "";
@@ -28,59 +28,78 @@ $opt = array(
 $pdo = new PDO($dsn, $user, $pass, $opt);
 
 //для статических запросов query()
-//$stmt = $pdo->query("SELECT name FROM region_stage_1");
-//while ($row = $stmt->fetch()) {
-//    echo $row['name'] . "<br>";
-//}
+$stmt = $pdo->query("SELECT name FROM region_stage_1");
+while ($row = $stmt->fetch()) {
+    echo $row['name'] . "<br>";
+}
 
 //Позиционный плейсхолдер
-//$stmt = $pdo->prepare('SELECT name FROM region_stage_1 WHERE level_id = ?');
-//$stmt->execute(array($_POST['level']));
-//foreach ($stmt as $row) {
-//    echo $row['name'] . "</br>";
-//}
+$stmt = $pdo->prepare('SELECT name FROM region_stage_1 WHERE level_id = ?');
+$stmt->execute(array($_POST['level']));
+foreach ($stmt as $row) {
+    echo $row['name'] . "</br>";
+}
 
 //Именованный плейсхолдер
-//$stmt = $pdo->prepare('SELECT name FROM region_stage_1 WHERE level_id = :level_id');
-//$stmt->execute(array('level_id' => $_POST['level']));
-//foreach ($stmt as $row) {
-//    echo $row['name'] . "</br>";
-//}
+$stmt = $pdo->prepare('SELECT name FROM region_stage_1 WHERE level_id = :level_id');
+$stmt->execute(array('level_id' => $_POST['level']));
+foreach ($stmt as $row) {
+    echo $row['name'] . "</br>";
+}
 
 //PDO::FETCH_LAZY
-//$stmt = $pdo->prepare('SELECT name FROM region_stage_1 WHERE level_id = :level_id');
-//$stmt->execute(array('level_id' => $_POST['level']));
-//while ($row = $stmt->fetch(PDO::FETCH_LAZY)) {
-//    echo $row->name . "<br>";
-//}
+$stmt = $pdo->prepare('SELECT name FROM region_stage_1 WHERE level_id = :level_id');
+$stmt->execute(array('level_id' => $_POST['level']));
+while ($row = $stmt->fetch(PDO::FETCH_LAZY)) {
+    echo $row->name . "<br>";
+}
 
 //fetchColumn() используем, если запрашиваем только одно поле
-//$stmt = $pdo->prepare('SELECT name FROM region_stage_1 WHERE id = :id');
-//$stmt->execute(array('id' => $_POST['id']));
-//$name = $stmt->fetchColumn();
+$stmt = $pdo->prepare('SELECT name FROM region_stage_1 WHERE id = :id');
+$stmt->execute(array('id' => $_POST['id']));
+$name = $stmt->fetchColumn();
 
-//$stmt = $pdo->prepare('SELECT name FROM region_stage_1 WHERE level_id = :level_id');
-//$stmt->execute(['level_id' => $_POST['level']]);
-//$array = $stmt->fetchAll();
-//foreach ($array as $user) {
-//    echo $user['name'] . "<br>";
-//}
+$stmt = $pdo->prepare('SELECT name FROM region_stage_1 WHERE level_id = :level_id');
+$stmt->execute(['level_id' => $_POST['level']]);
+$array = $stmt->fetchAll();
+foreach ($array as $user) {
+    echo $user['name'] . "<br>";
+}
 
 //Получение колонки
-//$data = $pdo->query('SELECT name FROM region_stage_1')->fetchAll(PDO::FETCH_COLUMN);
-//printr($data);
+$data = $pdo->query('SELECT name FROM region_stage_1')->fetchAll(PDO::FETCH_COLUMN);
+printr($data);
 
 //Получение пар ключ-значение.
-//$data = $pdo->query('SELECT id, name FROM region_stage_1')->fetchAll(PDO::FETCH_KEY_PAIR);
-//printr($data);
+$data = $pdo->query('SELECT id, name FROM region_stage_1')->fetchAll(PDO::FETCH_KEY_PAIR);
+printr($data);
 
 
 //Получение всех строк, индексированных полем.
-//$data = $pdo->query('SELECT * FROM region_stage_1')->fetchAll(PDO::FETCH_UNIQUE);
-//printr($data);
+$data = $pdo->query('SELECT * FROM region_stage_1')->fetchAll(PDO::FETCH_UNIQUE);
+printr($data);
+
+//PDO и оператор LIKE
+$name = "%{$_POST['str']}%";
+$stmt = $pdo->prepare("SELECT name FROM areas WHERE name LIKE :name");
+$stmt->execute(array('name' => $name));
+foreach ($stmt as $row) {
+    echo $row['name'] . "<br>";
+}
+
+//PDO и оператор IN
+$arr = array(4,5);
+$in = str_repeat('?,', count($arr)-1) . '?';
+$sql = "SELECT * FROM areas WHERE utc_timezone IN ($in)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute($arr);
+$data = $stmt->fetchAll();
+printr($data);
+
+
 ?>
 
 <form action="" method="POST">
-    <input type="text" name="level">
+    <input type="text" name="str">
     <input type="submit" name="submit">
 </form>
