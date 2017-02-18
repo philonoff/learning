11bde90dad0a4cc70ea129e455405ae5a221351e
philonoff <philkrm@gmail.com>
2017-02-18 17:10:32 +0200

    commit

---
+++ dataBase/home/index.php
@@ -0,0 +1,48 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>News</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/jumbotron-narrow.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="container">
+    <div class="header clearfix">
+        <nav>
+            <ul class="nav nav-pills pull-right">
+                <li role="presentation" class="active"><a href="index.php">Главная</a></li>
+                <li role="presentation"><a href="#">Добавить новость</a></li>
+            </ul>
+        </nav>
+        <h3 class="text-muted">Новости</h3>
+    </div>
+
+    <div class="row marketing">
+        <div class="col-lg-12">
+            <table class="table table-hover">
+                <tr>
+                    <td width="85%">-----Новость-----</td>
+                    <td><a class="btn btn-default btn-sm" href="editnews.php?id=1">Редактировать</a></td>
+                </tr>
+                <tr>
+                    <td width="85%">-----Новость-----</td>
+                    <td><a class="btn btn-default btn-sm" href="editnews.php?id=1">Редактировать</a></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div> <!-- /container -->
+
+</body>
+</html>
+
